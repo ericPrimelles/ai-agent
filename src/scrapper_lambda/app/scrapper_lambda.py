4dd336e021a63c2d8583070c1e48b4c1dd8d8572
eric <eric.a.primelles96@gmail.com>
2025-07-06 16:47:02 -0400

    Using firefox

--- src/scrapper_lambda/app/scrapper_lambda.py
+++ src/scrapper_lambda/app/scrapper_lambda.py
@@ -68,7 +68,7 @@ async def lambda_routine(event, context):
                 pass
             
             async with async_playwright() as p:
-                browser = await p.chromium.launch(headless=True)
+                browser = await p.firefox.launch(headless=True)
                 context = await browser.new_context(
                 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
                 viewport={"width": 1280, "height": 800}
